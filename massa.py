5dbea8ec674ce460ef85856cb4b891a459a8f4e0
bryan-ifpr <bryan.ifpr@gmail.com>
2024-10-08 19:51:17 -0300

    Update massa.py

--- massa.py
+++ massa.py
@@ -1,5 +1,7 @@
 def converte_g_para_Kg(gramas, quilogramas):
     gramas = quilogramas / 1000
+    return gramas
 
 def converte_Kg_para_g(quilograms, gramas):
-        quilogramas = gramas * 1000
+    quilogramas = gramas * 1000
+    return quilogramas
