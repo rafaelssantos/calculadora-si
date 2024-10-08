a9890d8ad050d1c3aaee2503cb6cd38bd6656805
jairomadalosso <jairro7777@gmail.com>
2024-10-08 19:58:47 -0300

    modulo comprimento
    função para converter metro para quilometro
    
    função para converter quilometro para metro

---
+++ comprimento.py
@@ -0,0 +1,8 @@
+# função para converter metro para quilometro
+def convert_metro_para_km(metro):
+   return metro / 1000
+
+
+# funcao para converter quilometro para metro
+def convert_quilometro_para_metro(quilometro):
+   return quilometro / 1000
