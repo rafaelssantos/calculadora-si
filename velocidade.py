e13f189351c2384f46abba0a8c7bd6741223fec9
MayaraMenezes <mayara.pereiramenezes@gmail.com>
2024-10-08 20:35:30 -0300

    Velocidade
    Conversão kmps para mps
    Conversão  mps para  kmps

---
+++ velocidade.py
@@ -0,0 +1,7 @@
+def conversor_kmph(mps):
+        conversor_kmph = mps * 3.6
+        return conversor_kmph
+
+def conversor_mps(kmph):
+        conversor_mps = kmph / 3.6
+        return conversor_mps
